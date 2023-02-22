18cfb856f88a02a45de4966f21efbb8e178b270e
Camilo Guevara <c.guevarat@uniandes.edu.co>
2023-02-22 18:59:29 -0500

    Suscriptor inicial

--- views/views.py
+++ views/views.py
@@ -24,7 +24,7 @@ def process_message():
         # Connect to RabbitMQ and send message to appropriate queue
         connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host, port=rabbitmq_port, credentials=pika.PlainCredentials(username=rabbitmq_username, password=rabbitmq_password)))
         channel = connection.channel()
-        queue_name = f'{service}-queue'
+        queue_name = 'respuesta_ventas'#f'{service}-queue'
         channel.queue_declare(queue=queue_name)
         channel.basic_publish(exchange='', routing_key=queue_name, body=json.dumps(message))
         connection.close()
@@ -32,3 +32,21 @@ def process_message():
         return jsonify({'message': f'Message sent to {service}-queue'}), 200
     else:
         return jsonify({'error': 'No available microservices to process message'}), 404
+    
+@bp.route('/subscribe')
+def subscribe():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='respuesta_ventas')
+
+    def callback(ch, method, properties, body):
+        # procesa el mensaje recibido aquí
+        print("Mensaje recibido: ", body)
+
+    channel.basic_consume(queue='respuesta_ventas', on_message_callback=callback, auto_ack=True)
+
+    print('Escuchando en la cola: respuesta_ventas')
+    channel.start_consuming()
+
+    return 'Suscrito a la cola'
